6c9d47b36da0cf02eca1d83676b4269494713dad
Will Cohen <will.r.cohen@gmail.com>
2020-10-27 16:53:25 -0400

    feat: Basic react setup with one page showing sample text

---
+++ src/TestSnippetDisplayPage/TestSnippetDisplayPageComponent.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const TestSnippetDisplayPageComponent = ({}) =>
+    <div>
+        <h1>Show a list of snippets here!</h1>
+    </div>
+
+export default TestSnippetDisplayPageComponent
---
+++ src/TestSnippetDisplayPage/TestSnippetDisplayPageContainer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { connect } from "react-redux"
+import TestSnippetDisplayPageComponent from "./TestSnippetDisplayPageComponent"
+
+const stateToPropertyMapper = (state) => ({})
+
+const propertyToDispatchMapper = (state) => ({})
+
+export default connect
+(stateToPropertyMapper, propertyToDispatchMapper)
+(TestSnippetDisplayPageComponent)
