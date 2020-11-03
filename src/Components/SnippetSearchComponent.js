3cce2061cf7ca9c9ecddb1063482e1caa584a2e2
Will Cohen <will.r.cohen@gmail.com>
2020-11-03 13:49:50 -0500

    Search feat: Instantiate basic container and component

---
+++ src/Components/SnippetSearchComponent.js
@@ -0,0 +1,7 @@
+import React from "react"
+import {Link} from "react-router-dom";
+
+const SnippetSearchComponent = () =>
+    <div><h2>SnippetSearchHere</h2></div>
+
+export default SnippetSearchComponent
---
+++ src/Containers/SnippetSearchContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+import SnippetSearchComponent from "../Components/SnippetSearchComponent";
+
+const stateToPropertyMapper = (state) => ({
+
+})
+
+const propertyToDispatchMapper =(dispatch) => ({
+
+})
+
+export default connect (
+    stateToPropertyMapper,
+    propertyToDispatchMapper
+)
+(SnippetSearchComponent)
