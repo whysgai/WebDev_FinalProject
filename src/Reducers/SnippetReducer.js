ef4de484a67aeeadf41e8e2d6045128bf80b86ca
Will Cohen <will.r.cohen@gmail.com>
2020-11-04 10:26:34 -0500

    Search: Add local static snippet

---
+++ src/Reducers/SnippetReducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+    widgets: [
+        {
+            id: 1,
+            gistId: "1a",
+            creator: "somedude",
+            dateCreated: "Yesterday",
+            lastModified: "Today",
+            title: "LocalTestSnippet",
+            description: "A locally saved snippet to demo",
+            codeText: "console.log('Hello, world!')",
+            tags: ['JavaScript', 'Output'],
+            shareableURL: "",
+            privacy: false,
+            recommended: false
+        }
+    ],
+
+}
