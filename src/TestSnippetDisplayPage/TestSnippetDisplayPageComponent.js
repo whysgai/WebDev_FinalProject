b0d66040975930c9b1c9854b5660f909982b7e64
Will Cohen <will.r.cohen@gmail.com>
2020-11-08 14:35:18 -0500

    Test: work on git request

--- src/TestSnippetDisplayPage/TestSnippetDisplayPageComponent.js
+++ src/TestSnippetDisplayPage/TestSnippetDisplayPageComponent.js
@@ -15,11 +15,11 @@ const TestSnippetDisplayPageComponent = ({loginGithub, getGistsForUser, getGistB
         </ul>
         <button onClick={getGistById}>Get One Gist</button>
         <p>{!gist? "nothing here" : gist.id}</p>
-        <p>files type is {typeof gist.files}</p>
+        {/*<p>files type is {typeof gist.files}</p>*/}
         {/*This one works*/}
-        <span>{!gist? "nothing here" : console.log(gist.files)}</span>
-        <span>{!gist? "nothing here" : console.log("HEY" + gist.files[0])}</span>
-        <p></p>
+        {/*<span>{!gist? "nothing here" : console.log(gist.files)}</span>*/}
+        {/*<span>{!gist? "nothing here" : console.log("HEY" + gist.files[0])}</span>*/}
+        {/*<p></p>*/}
         <button onClick={() => getGistFile(gist.files["Private Gist Test 1"].raw_url)}>Get Gist File</button>
         <p>{!file? "nothing here" : file}</p>
 
