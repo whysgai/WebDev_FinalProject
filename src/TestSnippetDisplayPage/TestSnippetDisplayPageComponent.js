d7e0133f868000d5a5c0fb0ded59a287c5196dc1
Nic <nicholasbshepard@github.ccs.neu.edu>
2020-11-13 10:09:24 -0500

    File working correctly

--- src/TestSnippetDisplayPage/TestSnippetDisplayPageComponent.js
+++ src/TestSnippetDisplayPage/TestSnippetDisplayPageComponent.js
@@ -1,28 +1,27 @@
 import React from "react"
 
+var fileName = ""
 const TestSnippetDisplayPageComponent = ({loginGithub, getGistsForUser, getGistById, getGistFile, gists, gist, file}) =>
     <div>
         <h1>Show a list of snippets here!</h1>
         <button onClick={getGistsForUser}>Get User Gists</button>
         <ul>
-                {
-                        gists[0] &&
-                            gists.map((gist, index) =>
-                                <li>Gist Id: {gist.id} Owner: {gist.owner.login}</li>
-                            )
+            {
+                gists[0] &&
+                gists.map((gist, index) =>
+                              <li>Gist Id: {gist.id} Owner: {gist.owner.login}</li>
+                )
 
-                }
+            }
         </ul>
         <button onClick={getGistById}>Get One Gist</button>
-        <p>{!gist? "nothing here" : gist.id}</p>
-        {/*<p>files type is {typeof gist.files}</p>*/}
-        {/*This one works*/}
-        {/*<span>{!gist? "nothing here" : console.log(gist.files)}</span>*/}
-        {/*<span>{!gist? "nothing here" : console.log("HEY" + gist.files[0])}</span>*/}
-        {/*<p></p>*/}
-        <button onClick={() => getGistFile(gist.files["Private Gist Test 1"].raw_url)}>Get Gist File</button>
-        <p>{!file? "nothing here" : file}</p>
+        <p>{!gist ? "nothing here" : gist.id}</p>
 
+
+            {(typeof Object.keys(gist) !== 'undefined' && Object.keys(gist).length > 0) ?
+             fileName = Object.keys(gist.files) : "No gist file to pull" }
+            <button onClick={() => getGistFile(gist.files[fileName].raw_url)}>Get Gist File</button>
+            <p>{!file? "nothing here" : file}</p>
     </div>
 
 export default TestSnippetDisplayPageComponent
