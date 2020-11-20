12da9882539144e3fe6f1d6baec5f2cc301f3a9e
Will Cohen <will.r.cohen@gmail.com>
2020-11-19 21:21:31 -0500

    Feat: load single snippet from server on single_snippet component

--- src/Containers/SingleSnippetContainer.js
+++ src/Containers/SingleSnippetContainer.js
@@ -21,52 +21,17 @@ class SingleSnippetContainer extends React.Component {
     };
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        const snippetId = this.props.match.params.snippetId
-        if (snippetId && snippetId !== prevProps.match.params.snippetId) {
-            const snippetId = this.props.match.params.snippetId
-            this.props.findSnippetById(snippetId)
-            console.log("Update!")
-        }
-        if (this.props.snippet !== prevProps.snippet) {
-            const snippetId = this.props.match.params.snippetId
-            this.props.findSnippetById(snippetId)
-            console.log("Update!")
-        }
+
     };
 
     render () {
 
         return (
             <div>
-                <h1>Static Message</h1>
-                {
-                    console.log("Static log message!")
-                }
-                <h2>{this.props.snippet && this.props.snippet}</h2>
-
-                {
-                    console.log("SingleSnippetCon: ", this.props.snippet)
-                }
                 {
                     this.props.snippet &&
                         <SnippetContainer
                             snippet={this.props.snippet}
-                            // snippet={
-                            //     {
-                            //         id: 1,
-                            //         gistId: "1a",
-                            //         creator: "somedude",
-                            //         dateCreated: "Yesterday",
-                            //         lastModified: "Today",
-                            //         title: "LocalTestSnippet0",
-                            //         description: "A locally saved snippet to demo",
-                            //         codeText: "console.log('Hello, world.')",
-                            //         tags: 'JavaScript,Output',
-                            //         shareableURL: "",
-                            //         privacy: false,
-                            //         recommended: false
-                            //     }
-                            // }
                             edit={true}
                         />
                 }
@@ -76,6 +41,7 @@ class SingleSnippetContainer extends React.Component {
 }
 
 const stateToPropertyMapper = (state) => ({
+    snippet: state.snippetReducer.snippet,
     snippets: state.snippetReducer.snippets,
     gists: state.gistReducer.gists,
     users: state.userReducer.users
