65aba6d5831256fae4bd1c8e30f1b8785112574f
Saleh Alkhalifa <salehesam@gmail.com>
2020-12-07 22:08:25 -0500

    Connected Redux, diagnosing empty array of snippets

--- src/Containers/LandingPageContainer.js
+++ src/Containers/LandingPageContainer.js
@@ -1,44 +1,70 @@
 import React from "react";
+import {findAllUsers} from "../Actions/UserActions";
+import {connect} from "react-redux";
+import {findAllPublicSnippets} from "../Actions/SnippetActions";
 // import {connect} from "react-redux";
 // import fire from "./../config/db";
 
-export default class LandingPageContainer extends React.Component {
+class LandingPageContainer extends React.Component {
 
-    state = {
-        user: "alkhalifas"
-    };
+    constructor() {
+        super();
+        this.state = {
+            user: "alkhalifas",
+            // user: null,
+            snippets: []
+        };
+    }
+
 
 
 
     componentDidMount() {
+        this.props.findAllPublicSnippets()
+        // this.render();
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    componentDidUpdate() {
+        // this.props.findAllPublicSnippets()
+        this.render();
     }
 
     render() {
         return (
             <div>
-                {console.log(this.state.user)}
+                {console.log(this.state.snippets)}
                 {
-                    !this.state.user == null &&
-                        <h1>Not Null</h1>
+                    this.state.user !== null &&
+                    <div className="jumbotron text-center" style={{height: 200}}>
+                        <h1 className="display-4">Welcome back to CodeSaver!</h1>
+                    </div>
                 }
                 {
                     this.state.user == null &&
-                    <h1>Null</h1>
-                }
-                <div className="jumbotron text-center" style={{height: 275}}>
-                    <h1 className="display-4">Welcome to CodeSaver!</h1>
-                    <p className="lead">"We are on a mission to make your coding life simpler, more productive, and more efficient."</p>
+                    <div className="jumbotron text-center" style={{height: 275}}>
+                        <h1 className="display-4">Welcome to CodeSaver!</h1>
+                        <p className="lead">"We are on a mission to make your coding life simpler, more productive, and more efficient."</p>
                         <p className="lead">
                             <a className="btn btn-primary btn-lg m-2" href="#" role="button">About Us</a>
                             <a className="btn btn-primary btn-lg m-2" href="#" role="button">Learn More</a>
                             <a className="btn btn-primary btn-lg m-2" href="#" role="button">Join Now</a>
                         </p>
-                </div>
+                    </div>
+                }
+
             </div>
         )
     }
 }
+
+const stateToPropertyMapper = (state) => ({
+    snippets: state.snippetReducer.snippets
+})
+
+const propertyToDispatchMapper = (dispatch) => ({
+    findAllPublicSnippets: () => findAllPublicSnippets(dispatch)
+})
+
+export default connect(stateToPropertyMapper, propertyToDispatchMapper)
+(LandingPageContainer)
 
