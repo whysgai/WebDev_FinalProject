67e6bb45f9034f36cd7fc5f7b5b532b703a0d3bb
Saleh Alkhalifa <salehesam@gmail.com>
2020-12-13 16:38:10 -0500

    Working on nav bar

--- src/Containers/NavbarContainer.js
+++ src/Containers/NavbarContainer.js
@@ -1,17 +1,50 @@
 import React from "react";
 import { connect } from "react-redux";
 import NavbarComponent from "../Components/NavbarComponent";
+import {getCookie} from "../config/db";
+import {getUserByUID} from "../Actions/UserActions";
+
+class NavbarContainer extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { };
+    }
+
+    componentDidMount() {
+        let fireUID = getCookie("uid")
+        this.props.getUserByUID(fireUID)
+    }
+
+    render () {
+        return (
+            <div>
+                {
+                    <NavbarComponent
+                        activeUser={this.props.activeUser}
+                        // fireUID={this.props.fireUID}
+                    />
+                }
+            </div>
+        )
+    }
+
+
+
+}
+
 
 const stateToPropertyMapper = (state) => ({
+    activeUser: state.userReducer.activeUser
 
-})
+});
 
 const propertyToDispatchMapper =(dispatch) => ({
-
-})
+    getUserByUID: (uid) => getUserByUID(dispatch, uid),
+});
 
 export default connect (
     stateToPropertyMapper,
     propertyToDispatchMapper
 )
-(NavbarComponent)
+(NavbarContainer)
